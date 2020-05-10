548682811e1b7cce1abbb145c5f3510a74116ee5
lemon-Boy <59724701+lemon-Boy@users.noreply.github.com>
2020-05-10 23:05:59 +0800

    Add files via upload

---
+++ precache-manifest.f178f6de6982cb48bafac5dcbeec4f41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54ae6e5b2436b8d78f9bf832c6e7da3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff7210f6408d69d60437",
+    "url": "/static/css/main.55d72bbf.chunk.css"
+  },
+  {
+    "revision": "c301bc5028c7df644770",
+    "url": "/static/js/2.2b752fa2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2b752fa2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff7210f6408d69d60437",
+    "url": "/static/js/main.40e1e506.chunk.js"
+  },
+  {
+    "revision": "1249babb1f9002060aaf",
+    "url": "/static/js/runtime-main.6a2302b0.js"
+  }
+]);
